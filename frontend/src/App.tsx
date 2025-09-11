c5cb94f75beebac4d8489cd85c9daab2b2d795c1
SamMC-211 <sammc.211@gmail.com>
2025-09-11 10:54:27 -0500

    Added react-router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+// React router: link (doesnt need active styling)
+// What is this compared to routes??
+import { Link } from "react-router";
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+import Home from "./Home";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<App />}></Route>
+          <Route path="/Home" element={<Home />}></Route>
+        </Routes>
+      </Router>
+
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -17,6 +30,12 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <Link to="/Home">
+        <h2>
+          {/* This adds /Home to url doesn't route to page ask gpt */}
+          {/* <Link to="/Home">Sample Home Page</Link> */}
+        </h2>
+      </Link>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -29,7 +48,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+//why dont I need
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+// React Router
+import ReactDOM from "react-dom/client";
+
+// const root = document.getElementById("root");
+
+//why doesnt this work?
+// ReactDOM.createRoot(root).render (
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
+
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
