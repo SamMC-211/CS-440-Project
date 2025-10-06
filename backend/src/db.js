a9cf31a520811996fbae1e0f6fecd14a1cfcae2b
banach0358 <banach0358@uwlax.edu>
2025-10-06 18:40:28 -0500

    db update
    Updated user table, created room and appointments table, added three users to test with.

--- backend/src/db.js
+++ backend/src/db.js
@@ -25,13 +25,38 @@ const db = new sqlite3.Database('./projdb.sqlite', (err) => {
 db.serialize(() => {
     db.run(`
         CREATE TABLE IF NOT EXISTS users (
-        firstname TEXT NOT NULL,
-        lastname TEXT NOT NULL,
-        email TEXT NOT NULL PRIMARY KEY,
-        password TEXT NOT NULL
+        user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        first_name TEXT NOT NULL,
+        last_name TEXT NOT NULL,
+        email TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL,
+        is_active INTEGER NOT NULL DEFAULT 0, -- 0=false, 1=true
+        role TEXT NOT NULL -- 'user', 'provider', or 'dev'
+        )
+    `);
+
+    db.run(`
+        CREATE TABLE IF NOT EXISTS rooms (
+        room_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        room_num INTEGER NOT NULL UNIQUE
+        )
+    `);
+
+    db.run(`
+        CREATE TABLE IF NOT EXISTS appointments (
+        appt_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        provider_id INTEGER,
+        start_time TEXT NOT NULL, 
+        end_time TEXT NOT NULL, 
+        is_booked INTEGER NOT NULL DEFAULT 0, -- 0=false, 1=true
+        user_id INTEGER, 
+        room_id INTEGER NOT NULL, 
+        appt_type TEXT, 
+        status TEXT NOT NULL DEFAULT 'open', 
+        FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE SET NULL ON UPDATE CASCADE,
+        FOREIGN KEY (room_id) REFERENCES rooms(room_id) ON DELETE RESTRICT ON UPDATE CASCADE
         )
     `);
 });
-
 //Allows the db object to be used outside of this file
 module.exports = db;
